f7ee0fb0ef80923d32d769d4cfbedf00ae14e636
LvsalnNarayana <88507292+LvsalnNarayana@users.noreply.github.com>
2024-08-03 15:18:31 +0100

    calender grid created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { Typography } from "@mui/material";
+import React from "react";
+
+import Calender from "./Components/Calender";
 
 const App = () => {
-  return <Typography variant="h2">Event Management</Typography>;
+  return <Calender />;
 };
 
 export default App;
---
+++ src/Components/Calender.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import "./Calender.css";
+
+const Calender = () => {
+  return (
+    <section className="content">
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+      <div className="day" />
+    </section>
+  );
+};
+
+export default Calender;
